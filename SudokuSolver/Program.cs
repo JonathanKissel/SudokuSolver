cc46b2916668276f27cb15e61b3bdff61bb638bf
Nash Kissel <nashkissel@gmail.com>
2018-11-27 20:31:44 -0800

    Add project files.

---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,40 @@
+﻿using SudokuSolver.Strategies;
+using SudokuSolver.Workers;
+using System;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SudokuMapper sudokuMapper = new SudokuMapper();
+                SudokuBoardStateManager sudokuBoardStateManager = new SudokuBoardStateManager();
+                SudokuSolverEngine sudokuSolverEngine = new SudokuSolverEngine(sudokuBoardStateManager, sudokuMapper);
+                SudokuFileReader sudokuFileReader = new SudokuFileReader();
+                SudokuBoardDisplayer sudokuBoardDisplayer = new SudokuBoardDisplayer();
+
+                Console.WriteLine("Please enter the filename containing the Sudoku Puzzle:");
+                var filename = Console.ReadLine();
+
+                var sudokuBoard = sudokuFileReader.ReadFile(filename);
+                sudokuBoardDisplayer.Display("Initial State", sudokuBoard);
+
+                bool isSudokuSolved = sudokuSolverEngine.Solve(sudokuBoard);
+                sudokuBoardDisplayer.Display("Final State", sudokuBoard);
+
+                Console.WriteLine(isSudokuSolved 
+                    ? "The Sudoku Puzzle has successfully been solved!"
+                    : "Unfortunatley, current algorithm(s) were not enough to solve the current Sudoku Puzzle!");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                // In real world we would want to log / save this message
+                Console.WriteLine("{0} : {1}", "Sudoku Puzzle cannot be solved because there was an error:", ex.Message);
+            }
+        }
+    }
+}
